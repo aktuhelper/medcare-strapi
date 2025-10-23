a75f6b7c23d95da8baec3f9e21e86119dad09b9a
Khushbu Singh <singhkhushbu3200@gmail.com>
2025-10-24 00:12:01 +0530

    dffndcnf

--- plugin.js
+++ plugin.js
@@ -1,11 +1,18 @@
-export default {
-    'cloudinary-media-library': {
-      enabled: true,
-      config: {
-        cloudName: 'your-cloud-name',
-        apiKey: 'your-api-key',
-        encryptionKey: '32 chars encryption key',
+// ./config/plugins.js (or ./config/plugins.ts)
+
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
-      
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
     },
-  };
+  },
+});
