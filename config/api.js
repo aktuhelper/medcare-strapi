99b090b20633d04bf585d665b815db5ecbfd0e3a
Khushbu Singh <singhkhushbu3200@gmail.com>
2025-10-23 22:47:28 +0530

    sixth commit

--- config/api.js
+++ config/api.js
@@ -4,4 +4,13 @@ module.exports = {
     maxLimit: 100,
     withCount: true,
   },
+  cors: {
+    enabled: true,
+    origin: [
+      'https://medcare-2xlxs1mta-aktuhelpers-projects.vercel.app',
+      'http://localhost:3000',  // for local development, optional
+    ],
+    headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  },
 };
