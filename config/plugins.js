9190e669542f48fb37955397fbca2faf620db3cc
Khushbu Singh <singhkhushbu3200@gmail.com>
2025-10-24 11:53:08 +0530

    fmkfkmfkmf

--- config/plugins.js
+++ config/plugins.js
@@ -1,24 +1,22 @@
 // ./config/plugins.js
-const cloudinaryProvider = require('@strapi/provider-upload-cloudinary');
-
 module.exports = ({ env }) => {
- 
-
-  return {
-    upload: {
-      config: {
-        provider: 'upload-cloudinary',
-        providerPackage: cloudinaryProvider, // 👈 manually link provider package
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
+   
+  
+    return {
+      upload: {
+        config: {
+          provider: 'upload-cloudinary', // ✅ keep this
+          providerOptions: {
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+          },
+          actionOptions: {
+            upload: {},
+            delete: {},
+          },
         },
       },
-    },
+    };
   };
-};
+  
